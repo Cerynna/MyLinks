22480f2b34b5ed0f5d0dde3b9e2395d812bbc64c
Jeremy MARTIN <cerynna@gmail.com>
2018-03-08 02:53:04 +0100

    make it better

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-// Désactiver le rapport d'erreurs
-error_reporting(0);
 
-// Rapporte les erreurs d'exécution de script
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
-// Rapporter les E_NOTICE peut vous aider à améliorer vos scripts
-// (variables non initialisées, variables mal orthographiées..)
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
-// Rapporte toutes les erreurs à part les E_NOTICE
-// C'est la configuration par défaut de php.ini
-error_reporting(E_ALL & ~E_NOTICE);
-
-// Reporte toutes les erreurs PHP (Voir l'historique des modifications)
-error_reporting(E_ALL);
-
-// Reporte toutes les erreurs PHP
-error_reporting(-1);
-
-// Même chose que error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
 
 include("views/partials/head.php");
 include("views/partials/menu.php");
@@ -28,10 +7,10 @@ include("views/partials/menu.php");
 
 /*var_dump($_SERVER);*/
 
-require('Entity/Base.php');
+/*require('Entity/Base.php');
 require('Entity/Link.php');
 
-$base = new Base();
+$base = new Base();*/
 
 
 
